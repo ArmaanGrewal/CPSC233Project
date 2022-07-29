b566c68a7997be2cc3df4d05e7128c67e0de0fdc
jagvirboparai <jagvirboparai@localhost>
2022-07-28 19:30:36 -0600

    Created the FXML file to open the login window of the application and created a basic layout for the login window without any functionality.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,23 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("src/application/ApplicationScene.fxml"));
+			Scene scene = new Scene(root,450,225);
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Our Application");
+			primaryStage.setTitle("J & A Banking Institution");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
