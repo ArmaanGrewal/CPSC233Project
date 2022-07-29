518bc474b9a89c2317ba09152e174b3079fa0241
jagvirboparai <jagvirboparai@localhost>
2022-07-29 15:55:41 -0600

    Allowed the create account button to lead to the create account scene.

--- src/application/loginController.java
+++ src/application/loginController.java
@@ -1,8 +1,15 @@
 package application;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class loginController {
@@ -17,7 +24,17 @@ public class loginController {
 
     @FXML
     void createUserAccount(ActionEvent event) {
-    	
+    	FXMLLoader loader1 = new FXMLLoader();
+    	VBox root1 = new VBox();
+    	try {
+			root1 = loader1.load(new FileInputStream("src/application/CreateAccountScene.fxml"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	Scene createAccountScene = new Scene(root1, 450, 225);
+    	loginStage.setScene(createAccountScene);
     }
 
     @FXML
